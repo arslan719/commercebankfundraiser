5d6a2f0dff27351b245fe4462e1cdb2af05e7e3a
Muhammad Arslan <marslan@stumail.jccc.edu>
2022-09-18 14:20:21 -0500

    Initial commit

---
+++ src/main/java/com/commerce/commercebankfundraiser/CommercebankfundraiserApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.commercebankfundraiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercebankfundraiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercebankfundraiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerce/commercebankfundraiser/CommercebankfundraiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.commercebankfundraiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercebankfundraiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
