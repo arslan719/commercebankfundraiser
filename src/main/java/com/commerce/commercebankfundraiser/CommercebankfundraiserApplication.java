8fd86fdcade721e01496b5aa637c862a94c797e3
Bilal Khan <akabilalkhan@gmail.com>
2022-09-30 14:34:06 -0500

    Database error resolved

--- src/main/java/com/commerce/commercebankfundraiser/CommercebankfundraiserApplication.java
+++ src/main/java/com/commerce/commercebankfundraiser/CommercebankfundraiserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
 import java.util.Scanner;
 
 
@@ -17,7 +18,18 @@ public class CommercebankfundraiserApplication {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		SpringApplication.run(CommercebankfundraiserApplication.class, args);
-		Doner d = new Doner();
+
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/User", "root", "Bilal786");
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from users");
+			while(resultSet.next()){
+				System.out.println(resultSet.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 
 	}
